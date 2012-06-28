38d156241065e624d723d75457eeb4648ad73617
hoangnhien <huuhoangbk@gmail.com>
2012-06-29 06:56:51 +0700

    dev
    Signed-off-by: hoangnhien <huuhoangbk@gmail.com>

--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -1,112 +1,125 @@
+<?php
+/**
+ * Description of products.php
+ * 
+ * @author HoangNhien
+ * Jun 29, 2012
+ */
+?>
+<?php
+/*
+Template Name: Products Template
+*/
+?>
 <?php get_header(); ?>
 
-	<?php
-		
-		while(have_posts()) :
-			the_post();
-			$currentCat = get_the_category();
-			if(sizeof($$currentCat) == 0){
-				$currentPageId = get_the_ID();
-			}
-		endwhile;
-		if(sizeof($currentCat)>0){
-			$currentCatName = $currentCat[0]->cat_name;
-			$currentPageSlug = $currentCatName;
-		
-			$args=array(
-			'name' => "$currentPageSlug",
-			'post_type' => 'page'
-			);
-			$my_posts = get_posts($args);
-			$currentPageId = $my_posts[0]->ID;
-
-		} else { 
-			$currentPage = get_page($currentPageId);
-			$currentPageSlug = $currentPage->post_name;
-			$currentCatName = $currentPageSlug;
-		}
-		
-		
-		wp_reset_query();
-	
-		
-		
-		
-	?>
-
-
-
-
-
 <div id="left-contain">
-    <div class="category-title">NỔI BẬT</div>
-    <div class="separate"></div>
-    <div class="clear"></div>
-    
-    <div class="category-content-highlights">
-        <?php
-			wp_reset_query;
-            query_posts(array('category_name' => 'highlights', 'posts_per_page' => -1,'order' => 'DESC')); 	
-            $i = 0;
-        ?>
-        <?php while(have_posts()) : the_post(); ?>
-            
-            <a href="<?php the_permalink(); ?>">
-				<?php echo get_the_post_thumbnail(get_the_ID()); ?>
-				<?php the_title(); ?></a>
-			
-            
-        <?php
-            $i++;
-            if($i >= 5) break;
-        endwhile;
-        ?>
-		
-		       
-    </div>
+	<div class="hn-widget">
+		<h1>
+			<img src="<?php bloginfo('template_directory')?>/images/icons/widget-title.png" alt="Products" />
+			DANH MỤC SẢN PHẨM
+		</h1>
+		<section>
+		<p>MTXT - Laptop</p>
+		<ul>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+		</ul>
+		</section>
+	</div>
+	<div class="hn-widget">
+		<h1>
+			<img src="<?php bloginfo('template_directory')?>/images/icons/widget-title.png" alt="Products" />
+			SẢN PHẨM NỔI BẬT
+		</h1>
+		<section>
+		<p>MTXT - Laptop</p>
+		<ul>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+			<li>Laptop HP</li>
+		</ul>
+		</section>
+	</div>
 </div>
 
 <div id="right-contain">
-	<div class="category-title">Nail Fever / <span>Mẫu thiết kế</span></div>
-    <div class="separate"></div>
-    <div class="clear"></div>
-	<?php
-		wp_reset_query();
-		$currentPageId;
-		$featureImg = get_the_post_thumbnail($currentPageId);
-		if($featureImg == "") $featureImg = '<img src="' . get_bloginfo('template_directory') .'/images/default/sub-page-feature.jpg"/>';
-	
-	?>
-	
-	<div id="feature-img"><?php echo $featureImg;  ?></div>
-    
-    <div class="category-content">
-		<?php
-			wp_reset_query();
+	<div id="top-banner">
+		<img src="<?php bloginfo('template_directory')?>/images/banner.jpg" alt="top banner" />
+	</div>
+	<div class="hn-category-view">
+		<div class="hn-category-title-block">
+			<div class="hn-category-title">
+				<img src="<?php bloginfo('template_directory')?>/images/icons/category-view-title.png" alt="Products" />
+				MTTX-LAPTOP
+				<a href="hoangnhien.net">&gt;&gt; xem tất cả</a>
+			</div>
+			<div class="hn-category-filter">
+				<h1>HP IDS 14UMA U3 4430s base NB PC (i5) (QJ670AV-89507255)</h1>
+			</div>
 			
-            //query_posts(array('category_name' => "$currentCatName", 'posts_per_page' => -1,'order' => 'DESC')); 	
-            
-        ?>
-		<?php while(have_posts()) : the_post(); ?>
-		<article>
-			<header>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				<p class="post-time">Posted On: <?php the_time('F j, Y') ?></p>
-			</header>
-			<section>
-				<?php echo get_the_post_thumbnail(get_the_ID()); ?>
-				<?php the_content(); ?>
-				<div class="clear"></div>
-			</section>
-		</article>
-            
-            
-        <?php endwhile; ?>
-		
+		</div>
+		<section class="clearfix product-detail">
+			<div class="hn-product-gallery">
+				<img src="<?php bloginfo('template_directory')?>/images/contents/product-full.jpg" alt="Products" />
+			</div>
+			<div class="hn-product-description">
+				<p>- Intel Core i5 -2450M 2.5GHz</p>
+				<p>- DDRAM 2GB/1333</p>
+				<p>- HDD 750GB</p>
+				<p>- Card Reader 4.1</p>
+				<p>- Intel HD 3000</p>
+				<p>- 14.0" HD LED - HDMI - Webcam - Finger Print</p>
+				<p>- LAN 10/100/1000 - Wireless - Bluetooth</p>
+				<p>- Weight 2.1Kg - Battery 6 Cell</p>
+				<p style="color:#ed1c24">- OS Win 7 Basic 32bit </p>
+				<a href="#">CLICK VÀO ĐÂY ĐỂ ĐẶT HÀNG</a>
+			</div>
+			<div class="clear"></div>
+			<button class="hn-product-intro-button">Tổng quan về sản phẩm</button>
+			<button class="hn-product-info-button active">Thông số kỹ thuật</button>
+			<div class="hn-product-intro"  style="display:none">
+				
+			</div>
+			<div class="hn-product-info">
+				<p>
+				HP Envy 17 3D (17-1190N) có thiết kế mạnh mẽ với lớp vỏ bằng nhôm mỏng với những đường nét trông giống như của Apple MacBook Pro 17 inch, nhưng HP đã bổ sung thêm một sắc thái màu champagne và đặt tên đặt là Carbon Relic.
+				Bàn phím vẫn thiết kế theo kiểu chiclet với kích cỡ đẹp mắt nhưng quan trọng nhất đó là sự lợi ích của bàn phím số tích hợp. Các phím có một phản ứng tốt và bấm rất nhạy. Thậm chí nó còn tốt hơn khi bàn phím tiêu chuẩn được trang bị đèn nền, làm nó cải thiện đáng kể trong môi trường tối. Clickpad cảm ứng có độ nhạy và độ chính xác tốt, cho phép sử dụng hai ngón tay di chuyển và phóng to mà không gặp khó khăn.
+				Màn hình 17,3 inch có một tính năng nổi bật cung cấp độ phân giải 1920x1080 pixel và hỗ trợ hiển thị nội dung 3D độ nét cao. Màn hình sử dụng công nghệ hiển thị 3D Ultra BrightView cho phép hình ảnh không chỉ hiển thị rõ ràng, tươi sáng mà còn bổ sung độ sáng để bù đắp cho các hiệu ứng mờ nhìn thấy khi xem qua kính 3D. Tuy nhiên, màn hình lớn khiến máy trở nên lớn và nặng nề. Khi đóng lại, nó có bề ngang 415 mm, rộng 275 mm, chỉ mỏng 31,75 mm nhưng nặng đến 3,4 kg, quá lớn và nặng nếu như người dùng thường mang để di chuyển hàng ngày.
+				</p>
+			</div>
+		</section>
+		<section class="clearfix product-related">
+			<div class="section-title">
+				<h1>SẢN PHẨM CÙNG LOẠI</h1>
+			</div>
+			<div class="hn-product-item-short">
+				<h1>HP Pavilio G4-1314TU (A9M54PA)</h1>
+				<img src="<?php bloginfo('template_directory')?>/images/contents/product-thumbnail.jpg" alt="Products" />
+				<p><a href="#">[chi tiết...]</a></p>
+			</div>
+			
+			<div class="hn-product-item-short">
+				<h1>HP Pavilio G4-1314TU (A9M54PA)</h1>
+				<img src="<?php bloginfo('template_directory')?>/images/contents/product-thumbnail.jpg" alt="Products" />
+				<p><a href="#">[chi tiết...]</a></p>
+			</div>
+			
+			<div class="hn-product-item-short">
+				<h1>HP Pavilio G4-1314TU (A9M54PA)</h1>
+				<img src="<?php bloginfo('template_directory')?>/images/contents/product-thumbnail.jpg" alt="Products" />
+				<p><a href="#">[chi tiết...]</a></p>
+			</div>
+		</section>
 		
 	</div>
+	
+
 </div>
+
 <div class="clear"></div>
 
-
- <?php get_footer(); ?>
+<?php get_footer(); ?>
